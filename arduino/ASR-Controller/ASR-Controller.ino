747638c12978ab008fa2c960e9fe77d37ad6f4e4
Laurent Pitoiset <github@pitoiset.net>
2015-01-04 22:06:56 +0100

    ASR Controller initial commit

---
+++ arduino/ASR-Controller/ASR-Controller.ino
@@ -0,0 +1,20 @@
+/* Laurent Pitoiset 2015
+
+ASR controller
+display latest application messages from ASR application
+
+*/
+
+// include
+
+// initialize
+
+void setup() {
+  // set up the lcd display
+  
+  // print default message
+
+// loop
+void loop(){
+  
+}
