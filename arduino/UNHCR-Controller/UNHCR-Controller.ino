6215fbd0198510f54afdb8ea09528d9098966b90
Laurent Pitoiset <github@pitoiset.net>
2015-01-06 17:44:33 +0100

    json object parsed, not arrays

--- arduino/UNHCR-Controller/UNHCR-Controller.ino
+++ arduino/UNHCR-Controller/UNHCR-Controller.ino
@@ -19,11 +19,15 @@
 #include <Ethernet.h>
 #include <ArduinoJson.h> 
 // initialize
+;
+
+boolean apiCall = false;
+
+json_year = 9999;
 
 int lcdDisplay  = 1; // 1 for 2*16 lcd display
 boolean proxyRouting = true; // true for proxy
 String reply = "";  // returned by web server
-
 byte mac[] = { 
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char server[] = "proxy.unhcr.local";    // 
@@ -40,6 +44,7 @@ LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
 void setup() {
   // set up the lcd display
+  lcd.clear();
   // set up the number of columns and rows on the LCD 
   lcd.begin(16, 2); 
   // Print a message to the LCD.
@@ -54,37 +59,39 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+  if (apiCall) 
+  {
+    // start the Ethernet connection:
+    if (Ethernet.begin(mac) == 0) {
+      Serial.println("Failed to configure Ethernet using DHCP");
+      // no point in carrying on, so do nothing forevermore:
+      // try to congifure using IP address instead of DHCP:
+      Ethernet.begin(mac, ip);
+    }
+    // give the Ethernet shield a second to initialize:
+    delay(1000);
+    Serial.println("connecting...");
+    lcd.setCursor(0, 1);
+    lcd.print("connecting");
 
-  // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    // try to congifure using IP address instead of DHCP:
-    Ethernet.begin(mac, ip);
-  }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-  lcd.setCursor(0, 1);
-  lcd.print("connecting");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 8080)) {
-    Serial.println("connected");
-    lcd.setCursor(0, 1);
-    lcd.print("connected");
-    // Make a HTTP request:
-    client.println("GET http://popdata.unhcr.org/ HTTP/1.0");
-    client.println("Connection: close");
-    lcd.setCursor(0, 1);
-    lcd.print("connection closed");
-    client.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-    lcd.setCursor(0, 1);
-    lcd.print("connection failed");
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 8080)) {
+      Serial.println("connected");
+      lcd.setCursor(0, 1);
+      lcd.print("connected");
+      // Make a HTTP request:
+      client.println("GET http://data.unhcr.org/api/stats/persons_of_concern.json?year=2013&country_of_origin=AFG&country_of_residence=PAK HTTP/1.0");
+      client.println("Connection: close");
+      lcd.setCursor(0, 1);
+      lcd.print("connection closed");
+      client.println();
+    } 
+    else {
+      // kf you didn't get a connection to the server:
+      Serial.println("connection failed");
+      lcd.setCursor(0, 1);
+      lcd.print("connection failed");
+    }
   }
 }
 
@@ -93,44 +100,56 @@ void setup() {
 
 // loop
 void loop() {
-  
-  
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-    reply = String(reply + c);
+
+  if (apiCall) 
+  {
+    // if there are incoming bytes available 
+    // from the server, read them and print them:
+    while (client.available()) {
+      char c = client.read();
+      Serial.print(c);
+      reply = String(reply + c);
+    }
   }
-  
-//char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
-char json[] = "{\"year\":\"2013\",\"country_of_residence\":\"PAK\",\"country_of_residence_en\":\"Pakistan\",\"country_of_origin\":\"AFG\",\"country_of_origin_en\":\"Afghanistan\",\"refugees\":\"1615876\",\"asylum_seekers\":\"5323\",\"returned_refugees\":null,\"idps\":null,\"returned_idps\":null,\"stateless_persons\":null,\"others_of_concern\":null,\"total_population\":\"1621199\"}";
-//char json[] = "{\"year\":\"2013\"}";
-StaticJsonBuffer<200> jsonBuffer;
+  //char json[] = "[{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}, {\"sensor\":\"gps\",\"time\":1351824140,\"data\":[50.756080,21.302038]}]";
+  //char json[] ="{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+  //char json[] = "{\"year\":\"2013\",\"country_of_residence\":\"PAK\",\"country_of_residence_en\":\"Pakistan\",\"country_of_origin\":\"AFG\",\"country_of_origin_en\":\"Afghanistan\",\"refugees\":\"1615876\",\"asylum_seekers\":\"5323\",\"returned_refugees\":null,\"idps\":null,\"returned_idps\":null,\"stateless_persons\":null,\"others_of_concern\":null,\"total_population\":\"1621199\"}";
+  char json[] = "[{\"year\":\"2013\"} {\"year\":\"2014\"}]";
 
-JsonObject& root = jsonBuffer.parseObject(json);
-//JsonObject& root2 = jsonBuffer.parseArray(root);
-/*
+  StaticJsonBuffer<200> jsonBuffer;
+  //JsonObject& root = jsonBuffer.parseObject(json);
+  JsonArray& root = jsonBuffer.parseArray(json);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  /*
 const char* sensor = root["sensor"];
-long time          = root["time"];
-double latitude    = root["data"][0];
-double longitude   = root["data"][1];
-*/
+   long time          = root["time"];
+   double latitude    = root["data"][0];
+   double longitude   = root["data"][1];
+   */
 
-const char* json_year = root["year"];
-const char* json_CoR = root["country_of_residence"];
-const char* json_CoO = root["country_of_origin"];
-const char* json_refugees = root["refugees"];
+  //const char* json_year = root["year"];
+  /*
+  const char* json_CoR = root["country_of_residence"];
+   const char* json_CoO = root["country_of_origin"];
+   const char* json_refugees = root["refugees"];
+   //const char* json_total = root["total_population"];
+   */
+  lcd.clear();
+  // lcd.print(reply);
+  lcd.setCursor(0,0);
+  //lcd.print(String(json_year)+" "+String(json_CoO)+" -> "+String(json_CoR));
+  lcd.setCursor(0,1);
+  //lcd.print(String("R "+String(json_refugees)));
+  lcd.print(String(json_year));
+  // do nothing forevermore:
+  while(true);
 
-    // do nothing forevermore:
-    lcd.clear();
-    // lcd.print(reply);
-    lcd.setCursor(0,0);
-    lcd.print(String(json_year)+" "+String(json_CoO)+" -> "+String(json_CoR));
-    lcd.setCursor(0,1);
-    lcd.print(String("R "+String(json_refugees)));
-    while(true);
-  
 }
+
+
 
 
